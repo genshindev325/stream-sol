290b88cc1da806163a8b24dec96c0b453e38ddc8
genshindev325 <genshindev325@gmail.com>
2024-06-03 08:33:57 -0400

    update

--- src/app/video/[id]/page.tsx
+++ src/app/video/[id]/page.tsx
@@ -1,12 +1,57 @@
 "use server"; // This is a server component
 
+import type { Metadata, ResolvingMetadata } from "next";
 import { redirect } from "next/navigation";
 import { getVideoById } from "@/services/video";
 import VideoPage from "@/components/video/VideoPage";
 
 type Props = {
   params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const videoId = params.id;
+
+  let metadata = {};
+
+  try {
+    const data = await getVideoById({
+      videoId,
+    });
+
+    const previousImages = (await parent).openGraph?.images || [];
+
+    metadata = {
+      title: data.video.title,
+      description: data.video.description,
+      openGraph: {
+        type: "website",
+        title: data.video.title,
+        description: data.video.description,
+        images: [
+          `https://${data.video.thumbnail}.ipfs.dweb.link`,
+          ...previousImages,
+        ],
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: data.video.title,
+        description: data.video.description,
+        images: [
+          `https://${data.video.thumbnail}.ipfs.dweb.link`,
+          ...previousImages,
+        ],
+      },
+    };
+  } catch (err) {
+    console.error(err);
+  }
+  return metadata;
+}
 
 export default async function RecordingVideo({ params }: Props) {
   const videoId = params.id;
